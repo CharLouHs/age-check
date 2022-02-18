3b96d517ada41ecbae2c1873b164963f2a38a74c
CharLouHs <chacptzy258@gmail.com>
2022-02-18 15:59:37 +0800

    input the country and age for taiwan

---
+++ age-check/main.py
@@ -0,0 +1,8 @@
+country =input('請問你是哪一個國家')
+age=input('輸入年齡')
+age= int(age)
+if country == 'taiwan':
+    if age >=18:
+        print('you can get the drinving exam')
+    else:
+        print('you are not allowed to have the exam')
